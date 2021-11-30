0ef19130a7813a9dc2ca7bb33d7839f675c186d6
Shyuro <78839297+Shyuro@users.noreply.github.com>
2021-11-30 14:16:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from utilidades.utilidades import moeda
 CHAVE_API = "2130502582:AAGBpiQc382sfWvsGXU0EjYWX-YKbo62JvI"
 
 bot = telebot.TeleBot(CHAVE_API, threaded=False)
-bot.remove_webhook()
-bot.set_webhook(url=f' https://nfthunterbot.herokuapp.com/{CHAVE_API}')
-
 global crypto_list
 crypto_list = ['BNB', 'ROFI', 'USDT']
 
@@ -31,3 +28,4 @@ def adicionar(mensagem):
 
 
 bot.polling()
+bot.set_webhook(url=f' https://nfthunterbot.herokuapp.com/{CHAVE_API}')
