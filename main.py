28cacfa80cc493748a663be2c6889e6c9729cf8e
Shyuro <78839297+Shyuro@users.noreply.github.com>
2021-11-30 14:53:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import telebot
 from utilidades.utilidades import moeda
+import os
+from flask import Flask, request
 
+
+PORT = int(os.environ.get('PORT', 5000))
 CHAVE_API = "2130502582:AAGBpiQc382sfWvsGXU0EjYWX-YKbo62JvI"
+server = Flask(__name__)
 
 bot = telebot.TeleBot(CHAVE_API, threaded=False)
 global crypto_list
@@ -20,6 +25,7 @@ def responder(mensagem):
     global msg
     bot.send_message(mensagem.chat.id, moeda(f'{msg}'))
 
+
 @bot.message_handler(commands=['add'])
 def adicionar(mensagem):
     new_token = str(mensagem.text).replace('/add ', '')
@@ -27,5 +33,20 @@ def adicionar(mensagem):
     crypto_list.append(new_token)
 
 
-bot.polling()
-bot.set_webhook(url=f' https://nfthunterbot.herokuapp.com/{CHAVE_API}')
+@server.route('/' + CHAVE_API, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://nfthunterbot.herokuapp.com/' + CHAVE_API)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
